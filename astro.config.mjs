d0b27e3f523a62ec2c0e52ca278b2b638ee9eb47
Atila Fassina <atila@fassina.eu>
2023-01-20 18:34:55 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,6 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
   integrations: [tailwind()],
   adapter: vercel(),
 });
