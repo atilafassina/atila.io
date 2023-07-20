47e1ffe19f25eb752a06e880d0b6844fa6c59706
Atila Fassina <atila@fassina.eu>
2023-07-20 16:54:17 +0200

    update

--- src/lib/db.server.ts
+++ src/lib/db.server.ts
@@ -1,31 +1,24 @@
 import { articleListSchema, talkListSchema } from "./schemas"
-import { XataClient } from "./xata.codegen"
+import { createClient } from "@supabase/supabase-js"
 
-const xata = new XataClient({
-  apiKey: import.meta.env.XATA_API_KEY,
-})
+const supabase = createClient(
+  import.meta.env.SUPABASE_DB,
+  import.meta.env.SUPABASE_KEY
+)
 
 export const fetchArticles = async () => {
-  const resp = await fetch("https://api.atila.io/writings")
-  const records = await resp.json()
+  const { data: records } = await supabase
+    .from("articles")
+    .select("*")
+    .order("published_at", {
+      ascending: false,
+    })
+
   return articleListSchema.parse(records)
 }
 
 export const fetchAppearances = async () => {
-  const resp = await fetch("https://api.atila.io/talks")
-  const records = await resp.json()
+  const { data: records } = await supabase.from("talks").select("*")
 
   return talkListSchema.parse(records)
-}
-
-export const fetchPaginatedArticles = async () => {
-  const list = await xata.db.articles
-    .sort("published_at", "desc")
-    .getPaginated({
-      pagination: {
-        size: 10,
-      },
-    })
-
-  return list
 }
