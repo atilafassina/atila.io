2715527a96e5934b5867097cf652dc9c0b5420ad
Atila Fassina <atila@fassina.eu>
2023-11-15 09:54:48 +0100

    wip

--- astro.config.ts
+++ astro.config.ts
@@ -1,37 +1,39 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
-import { pages } from "./pages.codegen.json";
-import vercel from "@astrojs/vercel/edge";
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+// import sitemap from "@astrojs/sitemap"
+// import { pages } from "./pages.codegen.json"
+import vercel from "@astrojs/vercel/serverless"
 
 export default defineConfig({
   output: "server",
+  adapter: vercel({
+    edgeMiddleware: true,
+  }),
   site: "https://atila.io",
   integrations: [
     tailwind(),
-    sitemap({
-      changefreq: "monthly",
-      priority: 0.7,
-      lastmod: new Date(),
-      customPages: pages,
-      serialize: (item) => {
-        if (
-          /writing/.test(item.url) ||
-          /channel/.test(item.url) ||
-          /talks/.test(item.url)
-        ) {
-          /**
-           * type not working well for changefreq
-           */
-          // @ts-expect-error
-          item.changefreq = "weekly";
-          item.lastmod = new Date().toISOString();
-          item.priority = 1;
-        }
+    //   sitemap({
+    //     changefreq: "monthly",
+    //     priority: 0.7,
+    //     lastmod: new Date(),
+    //     customPages: pages,
+    //     serialize: (item) => {
+    //       if (
+    //         /writing/.test(item.url) ||
+    //         /channel/.test(item.url) ||
+    //         /talks/.test(item.url)
+    //       ) {
+    //         /**
+    //          * type not working well for changefreq
+    //          */
+    //         // @ts-expect-error
+    //         item.changefreq = "weekly"
+    //         item.lastmod = new Date().toISOString()
+    //         item.priority = 1
+    //       }
 
-        return item;
-      },
-    }),
+    //       return item
+    //     },
+    //   }),
   ],
-  adapter: vercel(),
-});
+})
---
+++ src/content/notes/first.md
@@ -0,0 +1,14 @@
+---
+title: First note
+description: Blablabla
+slug: first
+date: 2023-07-08
+---
+
+# This is my first note
+
+Good morning, let's talk a little bit about this.
+
+```ts
+console.log("Hello World")
+```
