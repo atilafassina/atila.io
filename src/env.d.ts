1f09e2350761f8edd0c31a6617ba239635aeef1a
Atila Fassina <atila@fassina.eu>
2024-02-29 16:27:43 +0100

    fix channel request

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,10 @@
 /// <reference types="astro/client" />
 interface ImportMetaEnv {
   readonly XATA_API_KEY: string
+  readonly SUPABASE_KEY: string
+  readonly SUPABASE_DB: string
+  readonly YOUTUBE_API_TOKEN: string
+  readonly YOUTUBE_CHANNEL_ID: string
 }
 
 interface ImportMeta {
