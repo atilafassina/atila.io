c0b3f6f281e9f3c732aeae4e986b4bc099d702a5
Atila Fassina <atila@fassina.eu>
2024-08-30 13:46:28 +0200

    add workshop redirect

--- astro.config.ts
+++ astro.config.ts
@@ -1,30 +1,37 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
-import { pages } from "./pages.codegen.json";
-import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+import sitemap from "@astrojs/sitemap"
+import { pages } from "./pages.codegen.json"
+import vercel from "@astrojs/vercel/serverless"
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   site: "https://atila.io",
-  integrations: [tailwind(), sitemap({
-    changefreq: "monthly",
-    priority: 0.7,
-    lastmod: new Date(),
-    customPages: pages,
-    serialize: item => {
-      if (/writing/.test(item.url) || /channel/.test(item.url) || /talks/.test(item.url)) {
-        /**
-         * type not working well for changefreq
-         */
-        // @ts-expect-error
-        item.changefreq = "weekly";
-        item.lastmod = new Date().toISOString();
-        item.priority = 1;
-      }
-      return item;
-    }
-  })],
-  adapter: vercel()
-});
+  integrations: [
+    tailwind(),
+    sitemap({
+      changefreq: "monthly",
+      priority: 0.7,
+      lastmod: new Date(),
+      customPages: pages,
+      serialize: (item) => {
+        if (
+          /writing/.test(item.url) ||
+          /channel/.test(item.url) ||
+          /talks/.test(item.url)
+        ) {
+          /**
+           * type not working well for changefreq
+           */
+          // @ts-expect-error
+          item.changefreq = "weekly"
+          item.lastmod = new Date().toISOString()
+          item.priority = 1
+        }
+        return item
+      },
+    }),
+  ],
+  adapter: vercel(),
+})
